6490f0d163cb1d9eaaae2c413e1456d73e034358
William Cotton <williamc@eurekasoft.com>
2025-02-10 08:46:11 -0600

    add slider test

--- TestApplication/Source/Main.cpp
+++ TestApplication/Source/Main.cpp
@@ -3,13 +3,14 @@
 
 class MockHttpClient : public HttpClient {
 public:
-    juce::String makeGetRequest(const juce::String& url) override {
-        return R"({
-            "userId": 1,
+    juce::String makeGetRequest(const juce::String &url) override {
+        auto lastPart = url.getLastCharacters(1);
+        return juce::String::formatted(R"({
+            "userId": "%s",
             "id": 1,
             "title": "Test Todo",
             "completed": false
-        })";
+        })", lastPart.toRawUTF8());
     }
 };
 
@@ -21,7 +22,6 @@ public:
     void runTest() override
     {
         beginTest ("JSON Request Test");
-
         {
             juce::ScopedJuceInitialiser_GUI juceInit;
             std::shared_ptr<juce::MessageManager> messageManager(juce::MessageManager::getInstance());
@@ -45,6 +45,41 @@ public:
                 std::cout << "component.resultText.getText() = " << resultText << std::endl;
                 expect(resultText.contains("Received JSON"));
                 expect(resultText.contains("userId"));
+                expect(resultText.contains("1"));
+            }
+
+            juce::DeletedAtShutdown::deleteAll();
+            messageManager.reset();
+        }
+
+        beginTest("Slider Change JSON Request Test");
+        {
+            juce::ScopedJuceInitialiser_GUI juceInit;
+            std::shared_ptr<juce::MessageManager> messageManager(
+                juce::MessageManager::getInstance());
+            messageManager->setCurrentThreadAsMessageThread();
+
+            {
+                JsonApiComponent component(std::make_unique<MockHttpClient>());
+                component.setSize(600, 400);
+
+                component.sliderComponent.slider.setValue(2);
+                component.requestButton.triggerClick();
+
+                // Use a timer to stop the message loop after processing messages
+                juce::Timer::callAfterDelay(1000, [messageManager]() {
+                messageManager->stopDispatchLoop();
+                });
+
+                // Run the message loop until stopped by the timer
+                messageManager->runDispatchLoop();
+
+                auto resultText = component.resultText.getText();
+                std::cout << "component.resultText.getText() = " << resultText
+                        << std::endl;
+                expect(resultText.contains("Received JSON"));
+                expect(resultText.contains("userId"));
+                expect(resultText.contains("2"));
             }
 
             juce::DeletedAtShutdown::deleteAll();
